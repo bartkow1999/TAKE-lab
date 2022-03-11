c1299db54fecbf8f5caf2ffe9aa347fc5e344260
Bartosz Kowalski <bartosz.t.kowalski@student.put.poznan.pl>
2022-03-11 18:31:02 +0100

    feat: add lab1 - servlets & JSP

---
+++ lab1 - Serwlety Java, JSP, JSTL/ServletsJSPJSTL/src/java/ai/beans/ColorBean.java
@@ -0,0 +1,53 @@
+package ai.beans;
+
+public class ColorBean {
+
+    private String foregroundColor;
+    private String backgroundColor;
+    private String ramka;
+
+    public ColorBean() {
+    }
+
+    /**
+     * @return the foregroundColor
+     */
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    /**
+     * @param foregroundColor the foregroundColor to set
+     */
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    /**
+     * @return the backgroundColor
+     */
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * @param backgroundColor the backgroundColor to set
+     */
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    /**
+     * @return the ramka
+     */
+    public String getRamka() {
+        return ramka;
+    }
+
+    /**
+     * @param ramka the ramka to set
+     */
+    public void setRamka(String ramka) {
+        this.ramka = ramka;
+    }
+}
